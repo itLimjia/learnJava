19e3396e1cb4f97194434a564b272fd5ce90a44d
itLimjia <1951854815@qq.com>
2018-09-29 21:33:28 +0800

    commit a license file and source code

---
+++ first-day-code/ArrayDemo.java
@@ -0,0 +1,15 @@
+class ArrayDemo
+{
+	public static void main(String[] args) 
+	{
+		char[] charSave = new char[3];
+		charSave[0] = 'A';
+		charSave[1] = 'B';
+		charSave[2] = 'C';
+		int i;
+		for(i=0;i<3;i++)
+		{
+			System.out.println(charSave[i]);
+		}
+	}
+}
